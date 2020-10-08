97081517cd8133e0d64bc4768dd1d708045ace63
swatimehla <70935875+swatimehla@users.noreply.github.com>
2020-10-08 00:51:38 -0700

    Add files via upload

---
+++ Hello_World.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWORLD
+{
+    public delegate void MyDelegate(object sender, EventArgs e);
+    public partial class Hello_World : Form
+    {
+        public Hello_World()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button = new Button();
+            button.Text = "Click Me";
+            button.Size = new Size(100, 50);
+            button.Location = new Point(150, 100);
+            this.Controls.Add(button);
+
+            // button.Click += new EventHandler(button1_click1);
+            button.Click += delegate (object sender1, EventArgs e1)
+            {
+                MessageBox.Show("Hello World");
+            };
+
+            button.Click += delegate (object sender2, EventArgs e2)
+            {
+                MessageBox.Show("Programming is Fun");
+            };
+        }
+
+        private void button1_click1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
+
+    }
+}
---
+++ ProgrammingIsFun.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice_WindowApp
+{
+    public delegate void MyDelagate(object s, EventArgs e);
+    public partial class ProgrammingIsFun : Form
+    {
+        public ProgrammingIsFun()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button = new Button();
+            button.Text = "Click Me";
+            button.Size = new Size(100, 50);
+            button.Location = new Point(150, 100);
+            this.Controls.Add(button);
+
+            // button.Click += new EventHandler(button1_click1);
+
+            button.Click += delegate (object sender1, EventArgs e1)
+            {
+                MessageBox.Show("programming is Fun");
+            };
+
+            button.Click += delegate (object sender2, EventArgs e2)
+            {
+                MessageBox.Show("This is Printing");
+            };
+        }
+
+        private void button1_click1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Programming is Fun");
+        }
+
+
+    }
+}
