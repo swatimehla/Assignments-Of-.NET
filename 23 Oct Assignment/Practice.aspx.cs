baaa9a71bdc49a6f238da5e7983abf453bf72ab9
swatimehla <70935875+swatimehla@users.noreply.github.com>
2020-10-25 04:44:28 -0700

    Add files via upload

---
+++ 23 Oct Assignment/Practice.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void button1_Click(object sender, EventArgs e)
+        {
+            inputtext.Text =textbox1.Text;
+        }
+
+        protected void button2_Click(object sender, EventArgs e)
+        {
+            if(radiobutton1.Checked)
+            {
+                showgender.Text ="you are"+ radiobutton1.Text;
+            }
+            else
+            {
+                showgender.Text = "you are" + radiobutton2.Text;
+            }
+           
+        }
+    }
+}
---
+++ 23 Oct Assignment/Radio_Buttons.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class Radio_Buttons : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            Label3.Text = "This is Green Color";
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+                Label1.Text = "This is Red color";
+        }
+
+
+        protected void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            Label2.Text = "This is Blue Color";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+            Label2.Text = "";
+            Label3.Text = "";
+            if(RadioButton1.Checked)
+            {
+                Label1.Text = "This is Red color Button";
+            }
+            else if(RadioButton2.Checked)
+            {
+                Label2.Text = "This is Blue Color";
+            }
+            else
+            {
+                Label3.Text = "This is Green color";
+            }
+        }
+    }
+}
