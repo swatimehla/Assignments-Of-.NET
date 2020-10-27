352b221a1f0a4ad66a6c3b72d0759d1ae95cfb2e
swatimehla <70935875+swatimehla@users.noreply.github.com>
2020-10-27 00:57:37 -0700

    Add files via upload

---
+++ Cookies_Assignment/CookieCollection_prac.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class CookieCollection_prac : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void button1_Click(object sender, EventArgs e)
+        {
+            //Create cookies
+            if (checkbox1.Checked)
+            {
+                Response.Cookies["category"]["Jeans"] = "You have selected Jeans with trigger cookies";
+            }
+            if (checkbox2.Checked)
+            {
+                Response.Cookies["category"]["T-shirt"] = "You have selected T-shirt with trigger cookies\n";
+            }
+            if (checkbox3.Checked)
+            {
+                Response.Cookies["category"]["Shirt"] = "You have selected Shirt with trigger cookies";
+            }
+            if (checkbox4.Checked)
+            {
+                Response.Cookies["category"]["Kurta"] = "You have selected Kurta with trigger cookies";
+            }
+            if (checkbox5.Checked)
+            {
+                Response.Cookies["category"]["Top"] = "You have selected Top with trigger cookies\n";
+            }
+
+            //fetching cookies
+            if (Request.Cookies["category"].Values.ToString() != null)
+            {
+                if (Request.Cookies["category"]["Jeans"] != null)
+                {
+                    label2.Text = label2.Text + Response.Cookies["category"]["Jeans"] + "  " + " ";
+                }
+                if (Request.Cookies["category"]["T-shirt"] != null)
+                {
+                    label2.Text = label2.Text + Response.Cookies["category"]["T-shirt"] + "  " + " ";
+                }
+                if (Request.Cookies["category"]["Shirt"] != null)
+                {
+                    label2.Text = label2.Text + Response.Cookies["category"]["Shirt"] + "  " + " ";
+                }
+                if (Request.Cookies["category"]["Kurta"] != null)
+                {
+                    label2.Text = label2.Text + Response.Cookies["category"]["Kurta"] + "  " + " ";
+                }
+                if (Request.Cookies["category"]["Top"] != null)
+                {
+                    label2.Text = label2.Text + Response.Cookies["category"]["Top"] + "\n " + " ";
+                }
+            }
+            else
+            {
+                label2.Text = "select any choice";
+            }
+        }
+    }
+}
---
+++ Cookies_Assignment/HttpCookies.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class CookiesDOTNET : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { 
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //create cookies 
+            HttpCookie sname = new HttpCookie("Student Name");
+            HttpCookie lname = new HttpCookie("Last Name");
+            HttpCookie eid = new HttpCookie("Emp_id");
+            HttpCookie tech = new HttpCookie("Technology");
+            HttpCookie batch= new HttpCookie("Batch");
+
+            //assign values to the cookies
+            sname.Value = "Swati";
+            lname.Value = "Mehla";
+            eid.Value = "5";
+            tech.Value = "DotNet";
+            batch.Value = "3";
+
+            //Add the cookies 
+            Response.Cookies.Add(sname);
+            Response.Cookies.Add(lname);
+            Response.Cookies.Add(eid);
+            Response.Cookies.Add(tech);
+            Response.Cookies.Add(batch);
+
+            //Fetching values of Cookies
+
+            Studentname.Text =sname.Name+" : "+ sname.Value;
+            LastName.Text =lname.Name+" : "+ lname.Value;
+            EmployeeId.Text =eid.Name+" : "+ eid.Value;
+            Technology.Text =tech.Name+" : "+ tech.Value;
+            Batch.Text =batch.Name+" : "+ batch.Value;
+
+        }
+    }
+}
