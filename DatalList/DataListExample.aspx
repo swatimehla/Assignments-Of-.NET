003880151ec5fd9b3daa4e1115c28969151ed9be
swatimehla <70935875+swatimehla@users.noreply.github.com>
2020-10-27 09:21:01 -0700

    Add files via upload

---
+++ DatalList/DataListExample.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DataListExample.aspx.cs" Inherits="WebApplication4.DataListExample" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>Datalist show items of datatable</p>
+            <asp:DataList ID="datalist1" runat="server">
+                <ItemTemplate>
+                    <asp:Table ID="table1" runat="server" CellPadding="2" border="1"> 
+                        <asp:TableRow>
+                            <asp:TableCell> <strong>FirstName</strong> <span><%#Eval("Fname") %></span> </asp:TableCell>
+                                 <asp:TableCell> <strong>LastName</strong>  <%# Eval("Lname") %> </asp:TableCell>
+                                 <asp:TableCell> <strong>Designation</strong> <%# Eval("Designation") %></asp:TableCell>
+                                 <asp:TableCell> <strong>Location</strong>  <%# Eval("Location") %> </asp:TableCell>
+                                 <asp:TableCell> <strong>Country</strong> <%# Eval("Country") %></asp:TableCell>
+                           
+                        </asp:TableRow>
+                    </asp:Table>
+                </ItemTemplate>
+            </asp:DataList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DatalList/DataListExample.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace WebApplication4
+{
+    public partial class DataListExample : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataTable Table = new DataTable();
+            Table.Columns.Add("Fname");
+            Table.Columns.Add("Lname");
+            Table.Columns.Add("Designation");
+            Table.Columns.Add("Location");
+            Table.Columns.Add("Country");
+
+            Table.Rows.Add(101, "Ram", "SE","Chennai","India");
+            Table.Rows.Add(102, "kajal", "HR","Delhi","India");
+            Table.Rows.Add(103, "sweta", "QA","Bangalor","India");
+            Table.Rows.Add(104, "sunali", "SE","Channai","India");
+            Table.Rows.Add(105, "sham", "JE", "Delhi", "India");
+            Table.Rows.Add(106, "akash", "SE", "Delhi", "India");
+            Table.Rows.Add(107, "Seema", "JE", "Channai", "India");
+            Table.Rows.Add(108, "shiva", "HR", "Bangalor", "India");
+            Table.Rows.Add(109, "Amit", "HR", "Delhi", "India");
+            Table.Rows.Add(110, "sunil", "SE", "Channai", "India");
+
+
+            
+
+            datalist1.DataSource = Table;
+            datalist1.DataBind();
+        }
+    }
+}
