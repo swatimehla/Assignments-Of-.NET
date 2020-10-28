325ee23030953dc358682cbc3a5ab506818223b5
swatimehla <70935875+swatimehla@users.noreply.github.com>
2020-10-28 00:44:24 -0700

    Add files via upload

---
+++ DtaGrid_Assignment/DataGrid_Prac.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DataGrid_Prac.aspx.cs" Inherits="WebApplication4.DataGrid_Prac" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Employee Table</h2>
+            <asp:DataGrid ID="datagrid1" runat="server">
+            </asp:DataGrid>
+            <br />
+            <h2>Student Table</h2>
+            <asp:DataGrid ID="datagrid2" runat="server">
+            </asp:DataGrid>
+            <br />
+            
+            <h2>Indian Cricket Team Table</h2>
+            <asp:DataGrid ID="datagrid3" runat="server">
+            </asp:DataGrid>
+            <br />
+            
+            <h2>Indian Railway Table</h2>
+            <asp:DataGrid ID="datagrid4" runat="server">
+            </asp:DataGrid>
+            <br />
+            
+            <h2>Bank Table</h2>
+            <asp:DataGrid ID="datagrid5" runat="server">
+            </asp:DataGrid>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DtaGrid_Assignment/DataGrid_Prac.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication4
+{
+    public partial class DataGrid_Prac : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection("Server=IN-5CG03284JT\\SQLEXPRESS;Database=Dxc; integrated security=true"))
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("select * from Employee1", con);
+                DataSet ds = new DataSet();
+                sda.Fill(ds);
+                datagrid1.DataSource = ds;
+                datagrid1.DataBind();
+
+                SqlDataAdapter sda1 = new SqlDataAdapter("select * from Student", con);
+                DataSet ds1 = new DataSet();
+                sda.Fill(ds1);
+                datagrid2.DataSource = ds1;
+                datagrid2.DataBind();
+
+                SqlDataAdapter sda2 = new SqlDataAdapter("select * from Indian_Cricket_Team", con);
+                DataSet ds2 = new DataSet();
+                sda.Fill(ds2);
+                datagrid3.DataSource = ds2;
+                datagrid3.DataBind();
+
+                SqlDataAdapter sda3 = new SqlDataAdapter("select * from Indian_Railway_System", con);
+                DataSet ds3 = new DataSet();
+                sda.Fill(ds3);
+                datagrid4.DataSource = ds3;
+                datagrid4.DataBind();
+
+                SqlDataAdapter sda4 = new SqlDataAdapter("select * from Bank_Table", con);
+                DataSet ds4 = new DataSet();
+                sda.Fill(ds4);
+                datagrid5.DataSource = ds4;
+                datagrid5.DataBind();
+            }
+        
+        }
+    }
+}
